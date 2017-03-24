c6853c8bed3cc03d59f17e8609fb285bfb3acb44
eloaders <eloaders@linux.pl>
2017-03-24 07:02:34 +0100

    Recode

--- template-part-head.php
+++ template-part-head.php
@@ -5,11 +5,18 @@
 
     <div class="container"  style="padding-top: 60px;">
         <div class="row clearfix">
-            <div class="col-md-12">
-                <div class="page-header">
-                    <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a><small><?php bloginfo( 'description' ); ?></small></h1>
+
+                <div class="col-lg-8">
+                    <div class="page-header">
+                        <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+                        <small><?php bloginfo( 'description' ); ?></small></h1>
+                    </div>
                 </div>
-            </div>
+                <div class="col-lg-4">
+                    
+
+                </div>
+
         </div>
     </div>
 
